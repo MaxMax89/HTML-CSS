b6d824ba5e54959697ceeef256bcb367666045ed
MaxMax89 <68055296+MaxMax89@users.noreply.github.com>
2024-02-06 21:01:54 +0300

    add adaptive for layout

--- layout_apartments_rent/inc/apartmets_rent.php
+++ layout_apartments_rent/inc/apartmets_rent.php
@@ -1,73 +1,78 @@
-<div class="apartments_block">
-    <div class="apartments_block_title"><h1>More Then 500+<br>Appartments For Rent</h1></div>
-    <div class="apartments_block_content">
+<div class="apartments_wrapper">
+    <div class="apartments_block">
+        <div class="apartments_block_title"><h1>More Then 500+<br>Appartments For Rent</h1></div>
+        <div class="apartments_block_content">
 
-		<?
-		$array =
-			[
+			<?
+			$array =
 				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_01.jpg'
-				],
-				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_02.jpg'
-				],
-				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_03.jpg'
-				],
-				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_04.jpg'
-				],
-				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_05.jpg'
-				],
-				[
-					'name' => 'Random Street, Apt B453, New York',
-					'price' => '$3,500',
-					'img' => 'img/apartments/apartment_06.jpg'
-				]
-			];
-		?>
-        <div class="apartments_block_product">
-			<? foreach ($array as $value): ?>
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_01.jpg'
+					],
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_02.jpg'
+					],
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_03.jpg'
+					],
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_04.jpg'
+					],
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_05.jpg'
+					],
+					[
+						'name' => 'Random Street, Apt B453, New York',
+						'price' => '$3,500',
+						'img' => 'img/apartments/apartment_06.jpg'
+					]
+				];
+			?>
+            <div class="apartments_block_product">
+				<? foreach ($array as $value): ?>
                 <div class="apartments_block_product_card">
-                    <div class="apartments_block_product_img">
-                        <a href="#"><img src="<?= $value['img'] ?>" alt="pic"></a>
-                    </div>
-                    <a href="#"><p class="apartments_block_product_name"><?= $value['name'] ?></p></a>
-                    <div class="apartments_block_product_price">
-                        <p><?= $value['price'] ?></p>
-                        <div class="apartments_block_product_price_icon">
-                            <img src="../img/apartments/logo_shower.png" alt="">
-                            <p>2BA</p>
-                            <p>2DB</p>
-                            <img src="img/apartments/logo_bed.png" alt="">
+                    <div class="apartments_block_product_item">
+                        <div class="apartments_block_product_img">
+                            <a href="#"><img src="<?= $value['img'] ?>" alt="img"></a></div>
+                        <div class="apartments_block_product_info">
+                        <a href="#"><p class="apartments_block_product_name"><?= $value['name'] ?></p></a>
+                        <div class="apartments_block_product_price">
+                            <p><?= $value['price'] ?></p>
+                            <div class="apartments_block_product_price_icon">
+                                <img src="img/apartments/logo_shower.png" alt="">
+                                <p>2BA</p>
+                                <p>2DB</p>
+                                <img src="img/apartments/logo_bed.png" alt="">
+                            </div>
+                        </div>
                         </div>
                     </div>
                 </div>
-			<? endforeach; ?>
+				<? endforeach; ?>
+            </div>
         </div>
-    </div>
-    <div class="apartments_block_form">
-        <div class="apartments_block_form_search">
-            <img src="img/apartments/logo_magnifier.png" alt="">
-            <form action="#">
-                <input type="text" placeholder="Search location">
-                <button>Search</button>
-            </form>
+        <div class="apartments_block_form">
+            <div class="apartments_block_form_search">
+                <img src="img/apartments/logo_magnifier.png" alt="">
+                <form action="#">
+                    <input type="text" placeholder="Search location">
+                    <button>Search</button>
+                </form>
+            </div>
+            <div class="all_apartments"><a href="#">View All Appartments</a></div>
         </div>
-        <button class="all_apartments">View All Appartments</button>
-    </div>
 
+    </div>
 </div>
 
 </html>
--- layout_apartments_rent/inc/our_company.php
+++ layout_apartments_rent/inc/our_company.php
@@ -15,16 +15,15 @@
         <h1 class="our_company_title">Our’s Company <br>Statistics </h1>
         <p>
             In this space market-facing, yet UI work flows ,
-            or bake it in. Red flag we need a recap by<br>eod, cob or what-
-            ever comes first agile at the end of the day. Not enough bandwidth<br>
+            or bake it in. Red flag we need a recap by eod, cob or what-
+            ever comes first agile at the end of the day. Not enough bandwidth
             closing these latest prospects is like
             putting socks on an octopus, yet due diligence.
         </p>
         <p>We need to get all stakeholders up to speed and in the
-            right place hard stop, or techno-<br>logically savvy or pre launch. Pro-sumer software commitment to the
-            cause offline this<br> discussion and wiggle room blue sky. Beef up teams were able to drive adoption
-            and<br>
-            awareness. Screw the pooch killing it.
+            right place hard stop, or techno-logically savvy or pre launch. Pro-sumer software commitment to the
+            cause offline this discussion and wiggle room blue sky. Beef up teams were able to drive adoption
+            and awareness. Screw the pooch killing it.
         </p>
     </div>
 
@@ -54,10 +53,18 @@
 		];
 	?>
     <div class="our_company_widget">
-		<? foreach ($arr as $value): ?>
-            <div class="our_company_widget_img">
-                <p class="our_company_widget_count"><?= $value['count'] ?></p>
-                <p class="our_company_widget_name"><?= $value['name'] ?></p>
+		<? foreach (array_chunk($arr, 2) as $value): ?>
+            <div class="our_company_widget_row">
+				<? foreach ($value
+
+							as $v): ?>
+                    <div class="our_company_widget_img">
+                        <div class="our_company_widget_item">
+                            <p class="our_company_widget_count"><?= $v['count'] ?></p>
+                            <p class="our_company_widget_name"><?= $v['name'] ?></p>
+                        </div>
+                    </div>
+				<? endforeach; ?>
             </div>
 		<? endforeach; ?>
     </div>
